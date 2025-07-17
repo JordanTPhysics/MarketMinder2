3ae0eeb2dec7cce23b187381c9499c224a80fbfe
Jordan Thijssen <stargazerdio1999@gmail.com>
2025-07-17 10:43:24 +0100

    remove unused links

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,9 @@ const Header = () => {
     >
       <div className='lg:flex lg:flex-row text-text font-serif justify-evenly'>
         <h1 className='lg:text-5xl font-bold text-xl text-text rounded-md hover:scale-110 mx-4 px-12 transition-all duration-300 ease-in-out'><Link href="/">MarketMinder</Link></h1>
-        <Link href="/blog" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-r-md transition-all duration-300 ease-in-out link-border' >Blog</Link>
-        <Link href="/tracker" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-l-md transition-all duration-300 ease-in-out link-border' >Tracker</Link>
-        <Link href="/cases" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-l-md transition-all duration-300 ease-in-out link-border' >Case Studies</Link>
+        {/* <Link href="/blog" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-r-md transition-all duration-300 ease-in-out link-border' >Blog</Link> */}
+        {/* <Link href="/tracker" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-l-md transition-all duration-300 ease-in-out link-border' >Tracker</Link> */}
+        {/* <Link href="/cases" className='lg:text-3xl -skew-x-6 text-lg mx-auto lg:px-4 px-12 hover:scale-110 rounded-l-md transition-all duration-300 ease-in-out link-border' >Case Studies</Link> */}
       </div>
       {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
       
